f04bbbbcdbd2f7fc99df28aab5c943be0e2e4789
idan <idandrd@gmail.com>
2020-11-08 15:49:04 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 function main() {
-
-  let isOnEasy = false
-  let easyVerbutton = document.querySelector("#eLink") ? document.querySelector("#eLink") : Array.from(document.querySelectorAll('a.pannelB2.rToAddp')).pop();;
+  let isOnEasy = false;
+  let easyVerbutton = document.querySelector("#eLink")
+    ? document.querySelector("#eLink")
+    : Array.from(document.querySelectorAll("a.pannelB2.rToAddp")).pop();
   let easyVerButtonHtml = easyVerbutton.innerHTML;
   easyVerbutton.removeAttribute("onclick");
 
@@ -10,29 +11,26 @@ function main() {
     function (event) {
       let easyVerLink = easyVerbutton.href;
       let easyVerTon = getAllUrlParams(easyVerLink).ton;
-      let step = parseFloat(easyVerTon) * 2
+      let step = parseFloat(easyVerTon) * 2;
       if (isOnEasy) {
-        step = step * -1
-        easyVerbutton.innerHTML = easyVerButtonHtml
+        step = step * -1;
+        easyVerbutton.innerHTML = easyVerButtonHtml;
       } else {
-        easyVerbutton.innerText = "גרסה קלה - חזרה למקורית"
+        easyVerbutton.innerText = "גרסה קלה - חזרה למקורית";
       }
-      
+
       let chordElements = [...document.querySelectorAll("td.chords span")];
-      
+
       for (i = 0; i < chordElements.length; i++) {
         let element = chordElements[i];
-        let transposed = transpose.shift(
-          element.innerText,
-          step
-        );
+        let transposed = transpose.shift(element.innerText, step);
         element.innerText = transposed;
       }
       event.preventDefault();
-      isOnEasy = !isOnEasy
+      isOnEasy = !isOnEasy;
     },
     false
   );
 }
 
-main()
+main();
